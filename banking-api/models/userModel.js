0ed1edd78a6a4c7b20940721a60f2b74a1263aff
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-02-27 23:38:14 +0530

    Cleanup

--- banking-api/models/userModel.js
+++ banking-api/models/userModel.js
@@ -4,32 +4,24 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    // required: [true, 'Please tell us your name!']
   },
   username: {
     type: String,
-    // required: [true, 'Please add your user name!']
   },
   password: {
     type: String,
-    // required: [true, 'Please provide a password'],
-    // minlength: 8,
-    // select: false
   },
   pan: {
     type: String,
-    // required: true
   },
   aadhaar: {
     type: String,
-    // required: true
   },
   address: {
     type: String,
   },
   email: {
     type: String,
-    // required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email']
@@ -56,17 +48,6 @@ const userSchema = new mongoose.Schema({
   dateOfOpening: {
     type: Date
   },
-  // passwordConfirm: {
-  //   type: String,
-  //   required: [true, 'Please confirm your password'],
-  //   validate: {
-  //     // This only works on CREATE and SAVE!!!
-  //     validator: function (el) {
-  //       return el === this.password;
-  //     },
-  //     message: 'Passwords are not the same!'
-  //   }
-  // }
 });
 
 const User = mongoose.model('User', userSchema);
